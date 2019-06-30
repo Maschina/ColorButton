7a7fee4e4ee7aa77fdd854aba291bbd3fca2640c
Robert Hahn <robert.hahn@mailbox.org>
2019-06-30 22:21:53 +0200

    Initial Commit

---
+++ ColorButton/ColorButton.h
@@ -0,0 +1,19 @@
+//
+//  ColorButton.h
+//  ColorButton
+//
+//  Created by Robert Hahn on 30.06.19.
+//  Copyright © 2019 Robert Hahn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ColorButton.
+FOUNDATION_EXPORT double ColorButtonVersionNumber;
+
+//! Project version string for ColorButton.
+FOUNDATION_EXPORT const unsigned char ColorButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorButton/PublicHeader.h>
+
+
